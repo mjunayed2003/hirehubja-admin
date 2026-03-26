4e8877855227ad09a8157a722db3c722c2d589b2
mjunayed2003 <junayedislammd8@gmail.com>
2026-03-26 15:37:16 +0600

    debug

--- src/layouts/Auth/Auth.jsx
+++ src/layouts/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import { Outlet } from "react-router-dom";
 import { ConfigProvider } from "antd";
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Main from "../layouts/Main/Main";
-// import Auth from "../layouts/Auth/Auth";
-// import SignIn from "../pages/Auth/SignIn";
+import Auth from "../layouts/Auth/Auth";
+import SignIn from "../pages/Auth/SignIn";
 import { routesGenerators } from "../utils/routesGenerators";
 import { dashboardItems } from "../constants/router.constants";
-// import ForgotPassword from "../pages/Auth/ForgotPassword";
-// import VerifyEmail from "../pages/Auth/VerifyEmail";
-// import ResetPassword from "../pages/Auth/ResetPassword";
+import ForgotPassword from "../pages/Auth/ForgotPassword";
+import VerifyEmail from "../pages/Auth/VerifyEmail";
+import ResetPassword from "../pages/Auth/ResetPassword";
 import AdminRoutes from "./AdminRoutes";
-// import NotFound from "../pages/NotFound/NotFound";
+import NotFound from "../pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -22,37 +22,37 @@ const router = createBrowserRouter([
     ),
     children: routesGenerators(dashboardItems),
   },
-  // {
-  //   path: "/auth",
-  //   element: <Auth />,
-  //   children: [
-  //     {
-  //       path: "/auth",
-  //       element: <Navigate to={"/auth/sign-in"} />,
-  //     },
-  //     {
-  //       path: "/auth/sign-in",
-  //       element: <SignIn />,
-  //     },
-  //     {
-  //       path: "/auth/forgot-password",
-  //       element: <ForgotPassword />,
-  //     },
-  //     {
-  //       path: "/auth/verify-email/:id",
-  //       element: <VerifyEmail />,
-  //     },
-  //     {
-  //       path: "/auth/reset-password",
-  //       element: <ResetPassword />,
-  //     },
+  {
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      // {
+      //   path: "/auth",
+      //   element: <Navigate to={"/auth/sign-in"} />,
+      // },
+      {
+        path: "/auth/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/auth/forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "/auth/verify-email/:id",
+        element: <VerifyEmail />,
+      },
+      {
+        path: "/auth/reset-password",
+        element: <ResetPassword />,
+      },
       
-  //   ],
-  // },
-  // {
-  //   path: "*",
-  //     element: <NotFound />,
-  // },
+    ],
+  },
+  {
+    path: "*",
+      element: <NotFound />,
+  },
 ]);
 
 export default router;
